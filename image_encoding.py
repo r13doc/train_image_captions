f1f25ab43980981d177b37b7685d95a3241a6859
den <r13doc@yandex.ru>
2023-08-09 22:51:37 +0300

    initial files

--- image_encoding.py
+++ image_encoding.py
@@ -4,46 +4,38 @@ from keras.applications.vgg16 import VGG16, preprocess_input
 import numpy as np
 import matplotlib.pyplot as plt
 
-# имена классов
+# имена классов для тестирования
 with open('class_names.txt', 'r') as clas:
     class_names = clas.readlines()
 class_names = [i.replace('\n', '') for i in class_names]
 
-path = os.path.join('images/')
-images = os.listdir(path)
-test_images = images[:1000]
-train_images = images[1000:]
-test_v = 'test_vectorizer.npy'
-train_v = 'train_vectorizer.npy'
 
+def encoding_procedure(test_v, train_v, path_file, test_img, train_img):
+    # Если данные обработаны мы просто загружаем их
+    if os.path.exists(test_v) and os.path.exists(train_v):
+        print('Подготовленные векторы модели загружены')
+        train_vec = np.load(train_v)
+        test_vec = np.load(test_v)
+        return train_vec, test_vec
 
-if os.path.exists(test_v) and os.path.exists(train_v):
-    print('Подготовленные векторы модели загружены')
-    train_vector = np.load(train_v)
-    test_vector = np.load(test_v)
+    else:
+        # Обработка изображений
+        print("Обработанные моделью векторы изображения не обнаружены")
 
-else:
-    # Обработка изображений
-    def prepare_image(img):
-        files = path + img
-        read = tf.io.read_file(files)
-        im = tf.io.decode_jpeg(read, channels=3)
-        im = tf.image.resize(im, [224, 224])
-        image_pre = preprocess_input(im)
-        return image_pre
+        def prepare_image(img):
+            files = path_file + img
+            read = tf.io.read_file(files)
+            im = tf.io.decode_jpeg(read, channels=3)
+            im = tf.image.resize(im, [224, 224])
+            image_pre = preprocess_input(im)
+            return image_pre
 
-    print("Загрузка модели")
-    model = VGG16(weights='imagenet',
-                  classifier_activation=None)
+        print("Загрузка модели!!")
+        model = VGG16(weights='imagenet',
+                      classifier_activation=None)
 
-    # Сохранение обработанных изображений в файл numpy
-    def vectorized(data, check_npy):
-        if os.path.exists(check_npy):
-            if check_npy == train_v:
-                print('Созданы тренировочные данные')
-            else:
-                pass
-        else:
+        # Сохранение обработанных изображений в файл numpy
+        def vectorized(data, file_numpy):
             for ind, i in enumerate(data):
                 prepare = prepare_image(i)
                 im = tf.reshape(prepare, shape=(1, 224, 224, 3))
@@ -52,28 +44,33 @@ else:
                     test_train = np.array(pred)
                 else:
                     test_train = np.append(test_train, pred, axis=0)
-            np.save(check_npy[:-4], test_train)
+            np.save(file_numpy, test_train)
+
+        print("Тестовая выборка")
+        vectorized(test_img, test_v)
+        print("Тренировочная выборка")
+        vectorized(train_img, train_v)
+
+        test_vec = np.load(test_v)
+        train_vec = np.load(train_v)
+        return train_vec, test_vec
 
 
-    vectorized(test_images, test_v)
-    vectorized(train_images, train_v)
-
-    train_vector = np.load(train_v)
-    test_vector = np.load(test_v)
-
-
-# Тестирование данных после модели
+# Тестирование данных
 if __name__ == '__main__':
-    count = 0
+    from main import test_images, path, test_image_vector
 
-    for file, pred_vector in zip(test_images, test_vector):
+    count = 0
+    rand_num = np.random.choice(len(test_images), 1)
+
+    for file, pred_vector in zip(test_images, test_image_vector):
         image = plt.imread(path + file)
         softmax = tf.nn.softmax(pred_vector)
         prev, prev2 = np.argmax(softmax), np.max(softmax)
         print(prev, prev2, class_names[prev])
         plt.imshow(image)
         plt.show()
-        plt.pause(0.8)
+        plt.pause(2.8)
         count += 1
         if count % 15 == 0:
             break
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from image_encoding import test_images, test_vector, train_images, train_vector
-from text_annotations import dictionary, reverse_dictionary, vocabulary_size, captions
+import os
+from image_encoding import encoding_procedure
+import numpy as np
+
+path = os.path.join('images/')
+images = os.listdir(path)
+test_images = images[:1000]
+train_images = images[1000:]
+test_v = 'data_files/test_vector.npy'
+train_v = 'data_files/train_vector.npy'
+
+# Извлечение признаков изображения с помощью VGG16 готовой модели до
+# последнего слоя не включая слой с softmax активатора
+train_image_vector, test_image_vector = encoding_procedure(test_v,
+                                                           train_v,
+                                                           path,
+                                                           test_images,
+                                                           train_images)
+
 
